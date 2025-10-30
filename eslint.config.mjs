f8b916d4504a741c21bd3262704eea51712ebab0
James Russell Orola <jrmorola@gmail.com>
2025-10-30 21:06:58 +0800

    feat: disable react/no-children-prop eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -23,6 +23,7 @@ const eslintConfig = [
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
+      "react/no-children-prop": "off",
     },
   },
 ];
