d6f8c215bdcf82bac17992989d1de8115e6fb78c
stackd dev <dev@stackdapp.co>
2025-12-24 22:57:18 +0800

    tx signing fixed

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -13,7 +13,6 @@ export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <PrivyProvider
       appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
-      clientId={process.env.NEXT_PUBLIC_PRIVY_CLIENT_ID!}
       config={{
         embeddedWallets: {
           ethereum: {
